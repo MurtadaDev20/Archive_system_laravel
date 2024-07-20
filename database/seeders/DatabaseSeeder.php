4fdf2015c41ea4c45f95f892d7d913f6167bc023
Murtada Luqman <96062205+MurtadaDev20@users.noreply.github.com>
2024-07-20 23:59:16 +0300

    i used npm rundev

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,8 +21,8 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        // $this->call(RolesTableSeeder::class);
-        // $this->call(UsersTableSeeder::class);
+        $this->call(RolesTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
         $this->call([StatusesTableSeeder::class,]);
     }
 }
